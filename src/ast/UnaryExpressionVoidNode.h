9783545051f16483b36dcbcd6f3f4a9aa0c746f8
parkjaeman <jaeman1.park@samsung.com>
2015-09-09 18:01:05 +0900

    Add file UnaryExpressionVoidNode.h
    Former-commit-id: ec5288777696b6b1d63d1f6913c94060b3b802ce

---
+++ src/ast/UnaryExpressionVoidNode.h
@@ -0,0 +1,43 @@
+#ifndef UnaryExpressionVoidNode_h
+#define UnaryExpressionVoidNode_h
+
+#include "ExpressionNode.h"
+
+namespace escargot {
+
+class UnaryExpressionVoidNode : public ExpressionNode {
+public:
+    friend class ESScriptParser;
+    UnaryExpressionVoidNode(Node* argument)
+        : ExpressionNode(NodeType::UnaryExpressionVoid)
+    {
+        m_argument = argument;
+    }
+
+    ESValue executeExpression(ESVMInstance* instance)
+    {
+        //www.ecma-international.org/ecma-262/6.0/index.html#sec-unary-minus-operator
+        ESValue v;
+        try {
+            v = m_argument->executeExpression(instance);
+        } catch(const ESValue& e) {
+            if((m_argument->type() == Identifier || m_argument->type() == IdentifierFastCase || m_argument->type() == IdentifierFastCaseWithActivation) && e.isESPointer() && e.asESPointer()->isESObject() && e.asESPointer()->asESObject()->constructor() == ESValue(instance->globalObject()->referenceError())) {
+
+            } else {
+                throw e;
+            }
+        }
+
+        if (v.isNumber())
+            return ESValue();
+        else
+            RELEASE_ASSERT_NOT_REACHED();
+    }
+
+protected:
+    Node* m_argument;
+};
+
+}
+
+#endif
