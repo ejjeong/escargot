6698e5e45406ae06cbd807df0f8069cf88edcf54
parkjaeman <jaeman1.park@samsung.com>
2015-09-18 14:34:54 +0900

    Implement generateExpressionByteCode for ConditionalExpression
    Former-commit-id: 4624e01ca61f9a6dd42b7451051be46a116bca20

--- src/ast/ConditionalExpressionNode.h
+++ src/ast/ConditionalExpressionNode.h
@@ -25,6 +25,23 @@ public:
             return m_alternate->executeExpression(instance);
     }
 
+    virtual void generateExpressionByteCode(CodeBlock* codeBlock, ByteCodeGenereateContext& context)
+    {
+        m_test->generateExpressionByteCode(codeBlock, context);
+        codeBlock->pushCode(JumpIfTopOfStackValueIsFalse(SIZE_MAX), this);
+        size_t jumpPosForTestIsFalse = codeBlock->lastCodePosition<JumpIfTopOfStackValueIsFalse>();
+
+        m_consequente->generateExpressionByteCode(codeBlock, context);
+        JumpIfTopOfStackValueIsFalse* jumpForTestIsFalse = codeBlock->peekCode<JumpIfTopOfStackValueIsFalse>(jumpPosForTestIsFalse);
+        codeBlock->pushCode(Jump(SIZE_MAX), this);
+        size_t jumpPosForEndOfConsequence = codeBlock->lastCodePosition<Jump>();
+
+        jumpForTestIsFalse->m_jumpPosition = codeBlock->currentCodeSize();
+        m_alternate->generateExpressionByteCode(codeBlock, context);
+        Jump* jumpForEndOfConsequence = codeBlock->peekCode<Jump>(jumpPosForEndOfConsequence);
+        jumpForEndOfConsequence->m_jumpPosition = codeBlock->currentCodeSize();
+    }
+
 protected:
     ExpressionNode* m_test;
     ExpressionNode* m_consequente;
