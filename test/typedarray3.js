9c13c24828e55ad0e11f978f0e2bdca580d21212
parkjaeman <jaeman1.park@samsung.com>
2015-09-24 10:24:30 +0900

    Add unittest for TypedArray.prototype.subarray
    Former-commit-id: 7629406376da9fa00b8b1be7f09db6dea523a281

---
+++ test/typedarray3.js
@@ -0,0 +1,18 @@
+print("== new TypedArray ==");
+var a = new Int32Array(5);
+for (var i = 0; i < a.length; i++) {
+	a[i] = i;
+}
+print(a);
+print("length = " + a.length);
+for (var i = 0; i < a.length; i++) {
+	print(a[i]);
+}
+
+print("== subarray ==");
+var b = a.subarray(1, 3);
+print(b);
+print("length = " + b.length);
+for (var i = 0; i < b.length; i++) {
+	print(b[i]);
+}
