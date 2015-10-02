19d0efba15f940b6b12f6190712d8700bcfc01ad
Junyoung Cho <june0.cho@samsung.com>
2015-10-02 12:07:15 +0900

    Add missing js files in test262
    Former-commit-id: 67e9086c8a049babed60a4d001543508122bce29

---
+++ test/test262/external/contributions/Google/sputnik_conformance_modified/13_Function_Definition/13.2_Creating_Function_Objects/S13.2_A7_T1.js
@@ -0,0 +1,11 @@
+// Copyright 2011 Google Inc.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/**
+ * @description check if "caller" poisoning poisons
+ * hasOwnProperty too
+ * @onlyStrict
+ */
+"use strict";
+(function(){}).hasOwnProperty('caller');
+
