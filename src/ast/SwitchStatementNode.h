432614cff947e7647ec2ff33bba560e5f46ced78
Junyoung Cho <june0.cho@samsung.com>
2015-10-07 14:38:51 +0900

    Fix bugs in Switch statement: case ordering bug, equal -> strict equal
    Former-commit-id: 917680f635b874e4b6bd6c8e6c5d2ee460230581

--- src/ast/SwitchStatementNode.h
+++ src/ast/SwitchStatementNode.h
@@ -26,19 +26,19 @@ public:
         m_discriminant->generateExpressionByteCode(codeBlock, newContext);
 
         std::vector<size_t> jumpCodePerCaseNodePosition;
-        for(unsigned i = 0; i < m_casesA.size() ; i ++) {
-            SwitchCaseNode* caseNode = (SwitchCaseNode*) m_casesA[i];
-            codeBlock->pushCode(DuplicateTopOfStackValue(), this);
-            caseNode->m_test->generateExpressionByteCode(codeBlock, newContext);
-            codeBlock->pushCode(Equal(), this);
-            jumpCodePerCaseNodePosition.push_back(codeBlock->currentCodeSize());
-            codeBlock->pushCode(JumpAndPopIfTopOfStackValueIsTrue(SIZE_MAX), this);
-        }
         for(unsigned i = 0; i < m_casesB.size() ; i ++) {
             SwitchCaseNode* caseNode = (SwitchCaseNode*) m_casesB[i];
             codeBlock->pushCode(DuplicateTopOfStackValue(), this);
             caseNode->m_test->generateExpressionByteCode(codeBlock, newContext);
-            codeBlock->pushCode(Equal(), this);
+            codeBlock->pushCode(StrictEqual(), this);
+            jumpCodePerCaseNodePosition.push_back(codeBlock->currentCodeSize());
+            codeBlock->pushCode(JumpAndPopIfTopOfStackValueIsTrue(SIZE_MAX), this);
+        }
+        for(unsigned i = 0; i < m_casesA.size() ; i ++) {
+            SwitchCaseNode* caseNode = (SwitchCaseNode*) m_casesA[i];
+            codeBlock->pushCode(DuplicateTopOfStackValue(), this);
+            caseNode->m_test->generateExpressionByteCode(codeBlock, newContext);
+            codeBlock->pushCode(StrictEqual(), this);
             jumpCodePerCaseNodePosition.push_back(codeBlock->currentCodeSize());
             codeBlock->pushCode(JumpAndPopIfTopOfStackValueIsTrue(SIZE_MAX), this);
         }
@@ -48,11 +48,6 @@ public:
         codeBlock->pushCode(Jump(SIZE_MAX), this);
 
         size_t caseIdx = 0;
-        for(unsigned i = 0; i < m_casesA.size() ; i ++) {
-            SwitchCaseNode* caseNode = (SwitchCaseNode*) m_casesA[i];
-            codeBlock->peekCode<JumpAndPopIfTopOfStackValueIsTrue>(jumpCodePerCaseNodePosition[caseIdx++])->m_jumpPosition = codeBlock->currentCodeSize();
-            caseNode->generateStatementByteCode(codeBlock, newContext);
-        }
         for(unsigned i = 0; i < m_casesB.size() ; i ++) {
             SwitchCaseNode* caseNode = (SwitchCaseNode*) m_casesB[i];
             codeBlock->peekCode<JumpAndPopIfTopOfStackValueIsTrue>(jumpCodePerCaseNodePosition[caseIdx++])->m_jumpPosition = codeBlock->currentCodeSize();
@@ -61,6 +56,11 @@ public:
         if(m_default) {
             codeBlock->peekCode<Jump>(jmpToDefault)->m_jumpPosition = codeBlock->currentCodeSize();
             m_default->generateStatementByteCode(codeBlock, newContext);
+        }
+        for(unsigned i = 0; i < m_casesA.size() ; i ++) {
+            SwitchCaseNode* caseNode = (SwitchCaseNode*) m_casesA[i];
+            codeBlock->peekCode<JumpAndPopIfTopOfStackValueIsTrue>(jumpCodePerCaseNodePosition[caseIdx++])->m_jumpPosition = codeBlock->currentCodeSize();
+            caseNode->generateStatementByteCode(codeBlock, newContext);
         }
         size_t breakPos = codeBlock->currentCodeSize();
         newContext.consumeBreakPositions(codeBlock, breakPos);
