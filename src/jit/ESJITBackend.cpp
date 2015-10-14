2b6bf31add5ce0174cbf49c86e6930d3276c44cd
JIYUN YANG <ji.yang@samsung.com>
2015-10-14 19:06:11 +0900

    Diable i2d conversion (it produce invalid value)
    Former-commit-id: 3f4cca802b7372cea8710865c99b12e33ed9ed81

--- src/jit/ESJITBackend.cpp
+++ src/jit/ESJITBackend.cpp
@@ -287,11 +287,11 @@ LIns* NativeGenerator::nanojitCodegen(ESIR* ir)
     {
         INIT_ESIR(DoublePlus);
         INIT_BINARY_ESIR(DoublePlus);
-        if (leftType.isInt32Type())
-            left = m_out.ins1(LIR_i2d, left);
-        if (rightType.isInt32Type())
-            left = m_out.ins1(LIR_i2d, left);
-        ASSERT(left->isD() && right->isD());
+        // if (leftType.isInt32Type())
+        //     left = m_out.ins1(LIR_i2d, left);
+        // if (rightType.isInt32Type())
+        //     right = m_out.ins1(LIR_i2d, right);
+        // ASSERT(left->isD() && right->isD());
         return m_out.ins2(LIR_addd, left, right);
     }
     case ESIR::Opcode::StringPlus:
@@ -368,11 +368,11 @@ LIns* NativeGenerator::nanojitCodegen(ESIR* ir)
     {
         INIT_ESIR(DoubleMultiply);
         INIT_BINARY_ESIR(DoubleMultiply);
-        if (leftType.isInt32Type())
-            left = m_out.ins1(LIR_i2d, left);
-        if (rightType.isInt32Type())
-            right = m_out.ins1(LIR_i2d, right);
-        ASSERT(left->isD() && right->isD());
+        // if (leftType.isInt32Type())
+        //     left = m_out.ins1(LIR_i2d, left);
+        // if (rightType.isInt32Type())
+        //     right = m_out.ins1(LIR_i2d, right);
+        // ASSERT(left->isD() && right->isD());
         return m_out.ins2(LIR_muld, left, right);
     }
     case ESIR::Opcode::GenericMultiply:
