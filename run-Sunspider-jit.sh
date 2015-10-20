4cd3c1fcdb83e422ac96b7af7d46ef69f9e7e5db
Eunji Jeong <eun-ji.jeong@samsung.com>
2015-10-20 10:11:44 +0900

    Temporarily disable math-spectral-norm in originalCompiledFunctions list due to newly introduced bug after expanding the coverage of JIT
    Former-commit-id: b15815672af62630667f9ea60d40957c362e4ee0

--- run-Sunspider-jit.sh
+++ run-Sunspider-jit.sh
@@ -4,7 +4,7 @@ if [ "$#" -eq 1 ]
 then
   if [ "$1" = "-rcf" ]
   then
-    tests=("access-nsieve" "bitops-3bit-bits-in-byte" "bitops-bits-in-byte" "controlflow-recursive" "math-spectral-norm" "string-unpack-code")
+    tests=("access-nsieve" "bitops-3bit-bits-in-byte" "bitops-bits-in-byte" "controlflow-recursive" "string-unpack-code")
     for test in "${tests[@]}"; do
       ./escargot -rcf test/SunSpider/tests/sunspider-1.0.2/$test.js
     done
