79466d4be6593feaef335d35779eceaf02029bc1
wonyong.kim <wonyong.kim@samsung.com>
2015-11-19 18:42:33 +0900

    Add missing files to measure on android
    Former-commit-id: 0570b61fe1fbbdfcb94a483c91d9efc09a775982

---
+++ test/SunSpiderForAndroid/kraken.js
@@ -0,0 +1,3 @@
+var suitePath = "SunSpider/tests/kraken-1.1";
+var tests = [ "ai-astar", "audio-beat-detection", "audio-dft", "audio-fft", "audio-oscillator", "imaging-gaussian-blur", "imaging-darkroom", "imaging-desaturate", "json-parse-financial", "json-stringify-tinderbox", "stanford-crypto-aes", "stanford-crypto-ccm", "stanford-crypto-pbkdf2", "stanford-crypto-sha256-iterative"];
+var categories = [ "ai", "audio", "imaging", "json", "stanford" ];
