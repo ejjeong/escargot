1ffc64da8534a6b4fdb8912ced6ded22d7104767
Eunji Jeong <eun-ji.jeong@samsung.com>
2016-01-20 17:23:59 +0900

    Remove GPL-licensed hash function
    Former-commit-id: db1e81c43b01faddd4276774e2f0c09d8822c7ac

--- src/runtime/InternalAtomicString.h
+++ src/runtime/InternalAtomicString.h
@@ -89,119 +89,16 @@ template<> struct equal_to<escargot::InternalAtomicString> {
 
 }
 
-// this hash function is borrow from https://github.com/gcc-mirror/gcc/blob/master/libstdc%2B%2B-v3/libsupc%2B%2B/hash_bytes.cc
-
-inline std::size_t unaligned_load(const char* p)
-{
-    std::size_t result;
-    memcpy(&result, p, sizeof(result));
-    return result;
-}
-
-#if defined(ESCARGOT_32)
-// Implementation of Murmur hash for 32-bit size_t.
-inline size_t hashBytes(const void* ptr, size_t len, size_t seed)
-{
-    const size_t m = 0x5bd1e995;
-    size_t hash = seed ^ len;
-    const char* buf = static_cast<const char*>(ptr);
-
-    // Mix 4 bytes at a time into the hash.
-    while (len >= 4) {
-        size_t k = unaligned_load(buf);
-        k *= m;
-        k ^= k >> 24;
-        k *= m;
-        hash *= m;
-        hash ^= k;
-        buf += 4;
-        len -= 4;
-    }
-
-    // Handle the last few bytes of the input array.
-    switch (len) {
-    case 3:
-        hash ^= static_cast<unsigned char>(buf[2]) << 16;
-    case 2:
-        hash ^= static_cast<unsigned char>(buf[1]) << 8;
-    case 1:
-        hash ^= static_cast<unsigned char>(buf[0]);
-        hash *= m;
-    };
-
-    // Do a few final mixes of the hash.
-    hash ^= hash >> 13;
-    hash *= m;
-    hash ^= hash >> 15;
-    return hash;
-}
-
-#elif defined(ESCARGOT_64)
-
-// Loads n bytes, where 1 <= n < 8.
-inline std::size_t load_bytes(const char* p, int n)
-{
-    std::size_t result = 0;
-    --n;
-    do {
-        result = (result << 8) + static_cast<unsigned char>(p[n]);
-    } while (--n >= 0);
-    return result;
-}
-
-inline std::size_t shift_mix(std::size_t v)
-{
-    return v ^ (v >> 47);
-}
-
-// Implementation of Murmur hash for 64-bit size_t.
-inline size_t hashBytes(const void* ptr, size_t len, size_t seed)
-{
-    static const size_t mul = (((size_t) 0xc6a4a793UL) << 32UL)
-    + (size_t) 0x5bd1e995UL;
-    const char* const buf = static_cast<const char*>(ptr);
-
-    // Remove the bytes not divisible by the sizeof(size_t). This
-    // allows the main loop to process the data as 64-bit integers.
-    const int len_aligned = len & ~0x7;
-    const char* const end = buf + len_aligned;
-    size_t hash = seed ^ (len * mul);
-    for (const char* p = buf; p != end; p += 8) {
-        const size_t data = shift_mix(unaligned_load(p) * mul) * mul;
-        hash ^= data;
-        hash *= mul;
-    }
-    if ((len & 0x7) != 0) {
-        const size_t data = load_bytes(end, len & 0x7);
-        hash ^= data;
-        hash *= mul;
-    }
-    hash = shift_mix(hash) * mul;
-    hash = shift_mix(hash);
-    return hash;
-}
-
-#else
-
-// Dummy hash implementation for unusual sizeof(size_t).
-inline size_t hashBytes(const void* ptr, size_t len, size_t seed)
-{
-    size_t hash = seed;
-    const char* cptr = reinterpret_cast<const char*>(ptr);
-    for (; len; --len)
-    hash = (hash * 131) + *cptr++;
-    return hash;
-}
-
-
-#endif
-
 namespace std {
 template<> struct hash<std::pair<const char *, size_t> > {
     size_t operator()(std::pair<const char *, size_t> const &x) const
     {
-        size_t seed = static_cast<size_t>(0xc70f6907UL);
-        return hashBytes(x.first, x.second * sizeof(char), seed);
+        size_t length = x.second;
+        size_t hash = static_cast<size_t>(0xc70f6907UL);
+        const char* cptr = reinterpret_cast<const char*>(x.first);
+        for (; length; --length)
+            hash = (hash * 131) + *cptr++;
+        return hash;
     }
 };
 
