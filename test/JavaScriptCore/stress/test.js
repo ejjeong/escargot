855e254335e4fc9f512d0c2f00a4180b57e13cf2
Saebom Kim <sae-bom.kim@samsung.com>
2016-02-25 16:01:42 +0900

    __defineGetter__, __defineSetter__ in test.js should set enumerable, configurable as true
    Former-commit-id: 55bfdc92dd12476f24af12cd9e0bd4eec8fb5ca2

--- test/JavaScriptCore/stress/test.js
+++ test/JavaScriptCore/stress/test.js
@@ -77,12 +77,12 @@ function createProxy(arg) {
 }
 
 Object.defineProperty(Object.prototype, "__defineGetter__", {
-    value : function(prop, func) { Object.defineProperty(this, prop, {get: func}); },
+    value : function(prop, func) { Object.defineProperty(this, prop, {get: func, enumerable : true, configurable : true}); },
     enumerable : false
 });
 
 Object.defineProperty(Object.prototype, "__defineSetter__", {
-    value : function(prop, func) { Object.defineProperty(this, prop, {set: func}); },
+    value : function(prop, func) { Object.defineProperty(this, prop, {set: func, enumerable : true, configurable : true}); },
     enumerable : false
 });
 
