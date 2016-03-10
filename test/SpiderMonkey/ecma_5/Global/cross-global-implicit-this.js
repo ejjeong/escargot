d5bd3eee768f1dd8c45078abdcd85551356f3da9
Eunji Jeong <eun-ji.jeong@samsung.com>
2016-03-10 19:22:07 +0900

    Exclude tests that needs newGlobal() function from spidermonkey test
    Former-commit-id: 879bd539a6178a20235971c832f81d816b9e5dbc

--- test/SpiderMonkey/ecma_5/Global/cross-global-implicit-this.js
+++ test/SpiderMonkey/ecma_5/Global/cross-global-implicit-this.js
@@ -1,3 +1,4 @@
+// escargot-skip: needs newGlobal()
 // |reftest| skip-if(!xulRuntime.shell)
 // Any copyright is dedicated to the Public Domain.
 // http://creativecommons.org/licenses/publicdomain/
