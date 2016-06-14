6cf0d9d8762adc2a82aac62fe334182697ac9fff
Eunji Jeong <eun-ji.jeong@samsung.com>
2016-06-14 22:47:15 +0900

    Rename XFLAGS info ESCARGOT_XFLAGS in Makefile
    Former-commit-id: 1103ce369ae83b6c2d0b844d640cf1bd8a53edf7

--- build/Flags.mk
+++ build/Flags.mk
@@ -1,72 +1,72 @@
 #######################################################
 # common flags
 #######################################################
-CXXFLAGS_COMMON += -DESCARGOT
-CXXFLAGS_COMMON += -std=c++11
-CXXFLAGS_COMMON += -fno-rtti -fno-math-errno -I$(ESCARGOT_ROOT)/src/
-CXXFLAGS_COMMON += -fdata-sections -ffunction-sections
-CXXFLAGS_COMMON += -frounding-math -fsignaling-nans
-CXXFLAGS_COMMON += -Wno-invalid-offsetof
-CXXFLAGS_COMMON += -DUSE_ES6_FEATURE
+ESCARGOT_CXXFLAGS_COMMON += -DESCARGOT
+ESCARGOT_CXXFLAGS_COMMON += -std=c++11
+ESCARGOT_CXXFLAGS_COMMON += -fno-rtti -fno-math-errno -I$(ESCARGOT_ROOT)/src/
+ESCARGOT_CXXFLAGS_COMMON += -fdata-sections -ffunction-sections
+ESCARGOT_CXXFLAGS_COMMON += -frounding-math -fsignaling-nans
+ESCARGOT_CXXFLAGS_COMMON += -Wno-invalid-offsetof
+ESCARGOT_CXXFLAGS_COMMON += -DUSE_ES6_FEATURE
 
-LDFLAGS_COMMON += -lpthread
-LDFLAGS_COMMON += -lrt
+ESCARGOT_LDFLAGS_COMMON += -lpthread
+ESCARGOT_LDFLAGS_COMMON += -lrt
 
 #######################################################
 # flags for $(HOST) : linux / tizen*
 #######################################################
-CXXFLAGS_LINUX += -DENABLE_CODECACHE
-# CXXFLAGS_LINUX += -DENABLE_DTOACACHE
+ESCARGOT_CXXFLAGS_LINUX += -DENABLE_CODECACHE
+# ESCARGOT_CXXFLAGS_LINUX += -DENABLE_DTOACACHE
 
-CXXFLAGS_TIZEN += -DESCARGOT_SMALL_CONFIG=1 -DESCARGOT_TIZEN
+ESCARGOT_CXXFLAGS_TIZEN += -DESCARGOT_SMALL_CONFIG=1 -DESCARGOT_TIZEN
 
 #######################################################
 # flags for $(ARCH) : x64/x86/arm
 #######################################################
-CXXFLAGS_X64 += -DESCARGOT_64=1
-LDFLAGS_X64 =
+ESCARGOT_CXXFLAGS_X64 += -DESCARGOT_64=1
+ESCARGOT_LDFLAGS_X64 =
 
 # https://gcc.gnu.org/onlinedocs/gcc-4.8.0/gcc/i386-and-x86_002d64-Options.html
-CXXFLAGS_X86 += -DESCARGOT_32=1 -m32 -mfpmath=sse -msse -msse2
-LDFLAGS_X86 += -m32
+ESCARGOT_CXXFLAGS_X86 += -DESCARGOT_32=1 -m32 -mfpmath=sse -msse -msse2
+ESCARGOT_LDFLAGS_X86 += -m32
 
-CXXFLAGS_ARM += -DESCARGOT_32=1 -march=armv7-a -mthumb
-LDFLAGS_ARM =
+ESCARGOT_CXXFLAGS_ARM += -DESCARGOT_32=1 -march=armv7-a -mthumb
+ESCARGOT_LDFLAGS_ARM =
 
 #######################################################
 # flags for $(TYPE) : jit/interpreter
 #######################################################
-CXXFLAGS_INTERPRETER =
-CXXFLAGS_JIT = -DENABLE_ESJIT=1
+ESCARGOT_CXXFLAGS_INTERPRETER =
+ESCARGOT_CXXFLAGS_JIT = -DENABLE_ESJIT=1
 
 #######################################################
 # flags for $(MODE) : debug/release
 #######################################################
-CXXFLAGS_DEBUG += -O0 -g3 -D_GLIBCXX_DEBUG -fno-omit-frame-pointer -Wall -Wextra -Werror
-CXXFLAGS_DEBUG += -Wno-unused-but-set-variable -Wno-unused-but-set-parameter -Wno-unused-parameter
+ESCARGOT_CXXFLAGS_DEBUG += -O0 -g3 -D_GLIBCXX_DEBUG -fno-omit-frame-pointer -Wall -Wextra -Werror
+ESCARGOT_CXXFLAGS_DEBUG += -Wno-unused-but-set-variable -Wno-unused-but-set-parameter -Wno-unused-parameter
 
-CXXFLAGS_RELEASE += -O2 -g3 -DNDEBUG -fomit-frame-pointer -fno-stack-protector -funswitch-loops -Wno-deprecated-declarations
+ESCARGOT_CXXFLAGS_RELEASE += -O2 -g3 -DNDEBUG -fomit-frame-pointer -fno-stack-protector -funswitch-loops -Wno-deprecated-declarations
 ifneq (,$(findstring tizen,$(HOST)))
-  CXXFLAGS_RELEASE += -Os -finline-limit=64
+  ESCARGOT_CXXFLAGS_RELEASE += -Os -finline-limit=64
 endif
 
 #######################################################
 # flags for $(OUTPUT) : bin/shared_lib/static_lib
 #######################################################
-CXXFLAGS_BIN += -fvisibility=hidden -DESCARGOT_STANDALONE
-LDFLAGS_BIN += -Wl,--gc-sections
+ESCARGOT_CXXFLAGS_BIN += -fvisibility=hidden -DESCARGOT_STANDALONE
+ESCARGOT_LDFLAGS_BIN += -Wl,--gc-sections
 
-CXXFLAGS_SHAREDLIB += -fPIC
-LDFLAGS_SHAREDLIB += -ldl
+ESCARGOT_CXXFLAGS_SHAREDLIB += -fPIC
+ESCARGOT_LDFLAGS_SHAREDLIB += -ldl
 
-CXXFLAGS_STATICLIB += -fPIC
-LDFLAGS_STATICLIB += -Wl,--gc-sections
+ESCARGOT_CXXFLAGS_STATICLIB += -fPIC
+ESCARGOT_LDFLAGS_STATICLIB += -Wl,--gc-sections
 
 #######################################################
 # flags for LTO
 #######################################################
-CXXFLAGS_LTO += -flto -ffat-lto-objects
-LDFLAGS_LTO += -flto
+ESCARGOT_CXXFLAGS_LTO += -flto -ffat-lto-objects
+ESCARGOT_LDFLAGS_LTO += -flto
 
 #######################################################
 # flags for $(THIRD_PARTY)
@@ -74,45 +74,45 @@ LDFLAGS_LTO += -flto
 # icu
 ifeq ($(HOST), linux)
   ifeq ($(ARCH), x64)
-	CXXFLAGS_THIRD_PARTY += $(shell pkg-config --cflags icu-i18n)
-	LDFLAGS_THIRD_PARTY += $(shell pkg-config --libs icu-i18n)
+	ESCARGOT_CXXFLAGS_THIRD_PARTY += $(shell pkg-config --cflags icu-i18n)
+	ESCARGOT_LDFLAGS_THIRD_PARTY += $(shell pkg-config --libs icu-i18n)
   else ifeq ($(ARCH), x86)
-	CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/deps/x86-linux/include
-	LDFLAGS_THIRD_PARTY += -Ldeps/x86-linux/lib
-	LDFLAGS_THIRD_PARTY += -licuio -licui18n -licuuc -licudata
+	ESCARGOT_CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/deps/x86-linux/include
+	ESCARGOT_LDFLAGS_THIRD_PARTY += -Ldeps/x86-linux/lib
+	ESCARGOT_LDFLAGS_THIRD_PARTY += -licuio -licui18n -licuuc -licudata
   endif
 else ifneq (,$(findstring tizen_,$(HOST)))
   ifeq ($(ARCH), arm)
-	CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/deps/tizen/include
-	LDFLAGS_THIRD_PARTY += -Ldeps/tizen/lib/tizen-wearable-$(VERSION)-target-arm
-	LDFLAGS_THIRD_PARTY += -licuio -licui18n -licuuc -licudata
+	ESCARGOT_CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/deps/tizen/include
+	ESCARGOT_LDFLAGS_THIRD_PARTY += -Ldeps/tizen/lib/tizen-wearable-$(VERSION)-target-arm
+	ESCARGOT_LDFLAGS_THIRD_PARTY += -licuio -licui18n -licuuc -licudata
   else ifeq ($(ARCH), i386)
-	CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/deps/tizen/include
-	LDFLAGS_THIRD_PARTY += -Ldeps/tizen/lib/tizen-wearable-$(VERSION)-emulator-x86
-	LDFLAGS_THIRD_PARTY += -licuio -licui18n -licuuc -licudata
+	ESCARGOT_CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/deps/tizen/include
+	ESCARGOT_LDFLAGS_THIRD_PARTY += -Ldeps/tizen/lib/tizen-wearable-$(VERSION)-emulator-x86
+	ESCARGOT_LDFLAGS_THIRD_PARTY += -licuio -licui18n -licuuc -licudata
   endif
 endif
 
 # bdwgc
-CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/bdwgc/include/
+ESCARGOT_CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/bdwgc/include/
 ifeq ($(MODE), debug)
-  CXXFLAGS_THIRD_PARTY += -DGC_DEBUG
+  ESCARGOT_CXXFLAGS_THIRD_PARTY += -DGC_DEBUG
 endif
 
 # netlib
-CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/netlib/
+ESCARGOT_CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/netlib/
 
 # v8's fast-dtoa
-CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/double_conversion/
+ESCARGOT_CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/double_conversion/
 
 # rapidjson
-CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/rapidjson/include/
+ESCARGOT_CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/rapidjson/include/
 
 # yarr
-CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/yarr/
+ESCARGOT_CXXFLAGS_THIRD_PARTY += -I$(ESCARGOT_ROOT)/third_party/yarr/
 
 #######################################################
 # for printing TC coverage log
 #######################################################
-CXXFLAGS_TC = -DSTARFISH_TC_COVERAGE
+ESCARGOT_CXXFLAGS_TC = -DSTARFISH_TC_COVERAGE
 
